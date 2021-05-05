61023aa16cc44a76d1bc6ac7bce059e6f41b5d01
Hiro-1978 <hiromiuki11@gmail.com>
2021-05-05 10:21:12 +0700

    ๋Java Basic on text
    ex10_1 Method

---
+++ src/javabasic/Ex10_1.java
@@ -0,0 +1,21 @@
+package javabasic;
+
+public class Ex10_1 {
+    public static void main(String[] args){
+        Ex10_1 ex = new Ex10_1(); // ประกาศ Object ของตัวเองขึ้นมา
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+        ex.showMassage();
+    }
+    
+    public void showMassage(){
+        System.out.println("I Love Java");
+    }
+}
